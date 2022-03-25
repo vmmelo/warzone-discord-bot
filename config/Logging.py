dbc162f91ad8e7f6f05bae9c141e792d9133b901
Victor Miranda <vmmelo1@gmail.com>
2022-03-24 23:19:40 -0300

    fix: create logs path if not exists

--- config/Logging.py
+++ config/Logging.py
@@ -1,11 +1,14 @@
 import logging
-import os
+import os, os.path
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def saveLog(filename, content, log_type='info'):
+    if not os.path.exists("logs/"):
+        os.makedirs("logs/")
+
     logging.basicConfig(handlers=[logging.FileHandler('logs/' + filename, 'w+', 'utf-8')], level=40 if os.environ.get('APP_ENV') == 'production' else 10)
     logging.getLogger().addHandler(logging.StreamHandler())
     if log_type == 'error':
