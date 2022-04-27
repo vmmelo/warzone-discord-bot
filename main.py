88d2c711c691cbe99785669d1d59af79900797e8
Victor Miranda <vmmelo1@gmail.com>
2022-04-27 13:40:12 -0300

    feat: staging logic

--- main.py
+++ main.py
@@ -44,10 +44,13 @@ class WarzoneDiscordBot(discord.Client):
 
     async def check_and_notify_channels(self, update):
         msg_content = f'{update["text"]}'
-        if os.environ.get('APP_ENV') != 'production':
+        app_environ = os.environ.get('APP_ENV')
+        if app_environ == 'development':
             return
         for guild in client.guilds:
             channel = get(guild.text_channels, name='warzone-updates')
+            if app_environ == 'staging' and guild.name != 'A Raleta':
+                continue;
             if channel is None:
                 channel = await guild.create_text_channel('warzone-updates')
                 saveLog('WarzoneDiscordBot.log', f'created channel warzone-updates in guild {guild.name}')
